4618cfd49b8b0fc08a558b4a77d8dff4519582f3
Vaishnavi Nikam <142999402+Vaishnavinikam23@users.noreply.github.com>
2025-07-17 00:34:10 +0530

    Add files via upload

---
+++ innerhtml/index.js
@@ -0,0 +1,15 @@
+// innerHTML
+// let a = document.querySelector('.hero');
+// a.innerHTML='<b>Giga Chad</b>'
+// console.log(a)
+
+//outerHTML
+// let a = document.querySelector('.hero');
+// a.outerHTML='<b>Giga Chad</b>'
+// console.log(a.outerHTML)
+
+
+//pop ups
+//  alert("error")
+// prompt("server down")
+// confirm("are you 18+?")
